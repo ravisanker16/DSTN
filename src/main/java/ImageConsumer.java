7034f39de3a0c1c7dfd5a448eb645136b3464779
ravisanker16 <f20200142@goa.bits-pilani.ac.in>
2023-10-04 20:59:46 +0530

    add transfer images in kafka

---
+++ src/main/java/ImageConsumer.java
@@ -0,0 +1,80 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ImageConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(ImageConsumer.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer!");
+
+        String groupId = "my-java-application";
+        String topic = "initial";
+
+        // create Consumer Properties
+        Properties properties = new Properties();
+
+        // connect to Kafka broker(s)
+        properties.setProperty("bootstrap.servers", "10.50.1.3:9092");
+
+        // create consumer configs
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", ByteArrayDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // create a consumer
+        KafkaConsumer<String, byte[]> consumer = new KafkaConsumer<>(properties);
+
+        // subscribe to the topic
+        consumer.subscribe(Arrays.asList(topic));
+
+        try {
+            // poll for data
+            while (true) {
+                ConsumerRecords<String, byte[]> records = consumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord<String, byte[]> record : records) {
+                    log.info("Received message: Key - " + record.key());
+
+                    // Assuming the value is an image byte array (JPEG format)
+                    byte[] imageData = record.value();
+
+                    // Process and save the image to a file
+                    try {
+                        BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageData));
+                        String path = record.key().substring(0, record.key().length() - 4);
+                        String imagePath = path + "rcv" + ".jpg";
+
+                        File outputFile = new File(imagePath);
+                        outputFile.createNewFile();
+
+                        ImageIO.write(image, "jpg", outputFile);
+                        log.info("Saved image to: " + imagePath);
+                    } catch (IOException e) {
+                        log.error("Error while processing or saving the image", e);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("Unexpected exception in the consumer", e);
+        } finally {
+            consumer.close(); // close the consumer
+            log.info("The consumer is now gracefully shut down");
+        }
+    }
+}
---
+++ src/main/java/ImageProducer.java
@@ -0,0 +1,66 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ImageProducer {
+    private static final Logger log = LoggerFactory.getLogger(ImageProducer.class.getSimpleName());
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        log.info("Image Produce");
+
+        String path = "/Users/ravisanker/Documents/Acads/Academics_4_1/DSTN/Project/img/maa.jpg";
+        File imageFile = new File(path);
+        if (!imageFile.exists()) {
+            System.err.println("Image file does not exist at the specified path: " + path);
+
+            // Handle the error or exit gracefully
+        } else
+            System.out.println("File exits!");
+        // read image
+        try {
+            BufferedImage bImage = ImageIO.read(new File(path));
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ImageIO.write(bImage, "jpeg", bos);
+            byte[] data = bos.toByteArray();
+
+            // create producer properties
+            Properties properties = new Properties();
+            properties.setProperty("bootstrap.servers", "10.50.1.3:9092");
+
+            // set producer properties
+            properties.setProperty("key.serializer", StringSerializer.class.getName());
+            properties.setProperty("value.serializer", ByteArraySerializer.class.getName());
+
+            // create the producer
+            KafkaProducer<String, byte[]> producer = new KafkaProducer<>(properties);
+
+            // create a Producer Record
+            String topic = "initial";
+            ProducerRecord<String, byte[]> producerRecord =
+                    new ProducerRecord<>(topic, path, data);
+
+            // send the record (asynchronous)
+            producer.send(producerRecord);
+
+            // tell the producer to send all data and block until done (synchronous call)
+            producer.flush();
+
+            // close the producer
+            producer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
